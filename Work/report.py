7e6ee42e17282b5d7caa8d6db73348324fbe9c40
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 18:10:09 +0530

    Update to read prices from file Ex 2.6

--- Work/report.py
+++ Work/report.py
@@ -15,6 +15,26 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {'name': row[0],
+            'shares': int(row[1]),
+            'price': float(row[2])}
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    """
+    Read from prices file and return the list of stock prices
+
+    :param filename: Prices file
+    :returns list of stock prices
+    """
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                print(row, 'Invalid row to read')
+    return prices
