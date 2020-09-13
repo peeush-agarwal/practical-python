e0003a92d575c0023e11f698f2585999be267a0c
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 15:59:51 +0530

    Solve Ex 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,17 +1,19 @@
 # mortgage.py
 #
-# Exercise 1.7 (and 1.8)
+# Exercise 1.7 (and 1.8, 1.9)
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month_num = 0
-extra_payment = 1000 # $1000 per month
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
     monthly_payment = payment
-    if month_num < 12:
+    if extra_payment_start_month <= month_num <= extra_payment_end_month:
         monthly_payment += extra_payment
     # print(month_num, 'payment', monthly_payment)
     principal = principal * (1+rate/12) - monthly_payment
