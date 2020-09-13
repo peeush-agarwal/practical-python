4d8a8f2c0bfa67bc58b26c4e81e3fb1bb18210fa
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 17:35:09 +0530

    Solve 1.31 Error Handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,17 @@
 # pcost.py
 #
-# Exercise 1.30 (, 1.27)
+# Exercise 1.31 (, 1.30, 1.27)
 
 def portfolio_cost(filename):
     total_cost = 0.0
     with open(filename, 'rt') as f:
         headers = next(f)
         for line in f:
-            name, n_shares, cost = line.split(',')
-            total_cost += int(n_shares) * float(cost)
+            try:
+                name, n_shares, cost = line.split(',')
+                total_cost += int(n_shares) * float(cost)
+            except ValueError:
+                print('Error occured.')
     return total_cost
 
 total_cost = portfolio_cost('Data/portfolio.csv')
