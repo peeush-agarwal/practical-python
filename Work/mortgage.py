d177b51b4b2ff1ce062e64e74d5f7fee0ddcfb3e
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 15:52:39 +0530

    Solve Ex 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,14 +1,21 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.7 (and 1.8)
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month_num = 0
+extra_payment = 1000 # $1000 per month
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    monthly_payment = payment
+    if month_num < 12:
+        monthly_payment += extra_payment
+    # print(month_num, 'payment', monthly_payment)
+    principal = principal * (1+rate/12) - monthly_payment
+    total_paid = total_paid + monthly_payment
+    month_num += 1
 
-print('Total paid', total_paid)
+print('Total paid', total_paid, 'over', month_num)
