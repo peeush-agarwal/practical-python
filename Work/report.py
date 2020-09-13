4cfd039e5795fae1e0d3a5bfa556e03c4fa47df8
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 18:19:14 +0530

    Update to compute gain/loss Ex 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,7 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.7 (, 2.6, 2.4)
+import sys
 import csv
 
 def read_portfolio(filename):
@@ -36,5 +37,30 @@ def read_prices(filename):
             try:
                 prices[row[0]] = float(row[1])
             except IndexError:
-                print(row, 'Invalid row to read')
+                print('Warning:', row, 'Invalid row to read')
     return prices
+
+portfolio_filename = 'Data/portfolio.csv'
+prices_filename = 'Data/prices.csv'
+if len(sys.argv) == 3:
+    portfolio_filename = sys.argv[1]
+    prices_filename = sys.argv[2]
+
+portfolio = read_portfolio(portfolio_filename)
+prices = read_prices(prices_filename)
+
+total_purchase_price = 0.0
+total_current_price = 0.0
+for item in portfolio:
+    name = item['name']
+    n_shares = item['shares']
+    purchase_price = n_shares * item['price']
+    current_price = purchase_price
+    if name in prices:
+        current_price = n_shares * prices[name]
+    total_purchase_price += purchase_price
+    total_current_price += current_price
+
+print('Total purchase price', total_purchase_price)
+print('Total current price', total_current_price)
+print('Status:', 'Gain' if total_purchase_price < total_current_price else 'Loss')
