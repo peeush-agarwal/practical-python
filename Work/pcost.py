e14d0ce280c7f81874ce0b531a3eaeeb37a29827
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-09-13 17:33:21 +0530

    Solve 1.30 functions

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,15 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.30 (, 1.27)
 
-total_cost = 0.0
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        name, n_shares, cost = line.split(',')
-        total_cost += int(n_shares) * float(cost)
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            name, n_shares, cost = line.split(',')
+            total_cost += int(n_shares) * float(cost)
+    return total_cost
+
+total_cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost', total_cost)
